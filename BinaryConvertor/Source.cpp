a6f35646c12d84b68c658bce6b230a7d45395b16
GMargolin <91896446+GMargolin@users.noreply.github.com>
2021-10-04 20:43:10 +0300

    Добавьте файлы проекта.

---
+++ BinaryConvertor/Source.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	int number, digit = 0;
+	string result = "";
+	cout << "Input number please=";
+	cin >> number;
+	while (number > 0)
+	{
+		digit = number % 2;
+		result = to_string(digit) + result;
+		number /= 2;
+	}
+	cout << result;
+
+	return 0;
+}
